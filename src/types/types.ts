bb60e83027d6e767f248c3409ce1aa1e30dfb119
Rav Sispal <rav.sispal@ravsispal.com>
2024-10-25 09:27:23 +0100

    Add types

---
+++ src/types/types.ts
@@ -0,0 +1,18 @@
+
+export enum Direction {
+    NORTH = "N",
+    EAST = "E",
+    SOUTH = "S",
+    WEST = "W"
+};
+
+export type DirectionPayload = {
+    x: number;
+    y: number;
+    direction: Direction;
+};
+
+export type Coordinates = { 
+    x: number, 
+    y: number
+};
